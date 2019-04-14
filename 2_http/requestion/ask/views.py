e702bd3620ffa187a4199591bba5e100d2cd2b9f
Bogdan Salyp <bogdan@dotin.us>
2019-04-14 16:07:47 +0300

    Added title parameter to be passed from controller to view

--- 2_http/requestion/ask/views.py
+++ 2_http/requestion/ask/views.py
@@ -4,46 +4,62 @@ from django.template import loader
 
 def main(request):
     template = loader.get_template("index.html")
-    context = {}
+    context = {
+        "title": "New Question"
+    }
     return HttpResponse(template.render(context, request))
 
 
 def hot(request):
     template = loader.get_template("index.html")
-    context = {}
+    context = {
+        "title": "Hot Questions"
+    }
     return HttpResponse(template.render(context, request))
 
 
 def tag(request, tag_name):
     template = loader.get_template("tag.html")
-    context = {}
+    context = {
+        "title": "Questions by tag '{}'".format(tag_name)
+    }
     return HttpResponse(template.render(context, request))
 
 
 def question(request, question_id):
     template = loader.get_template("question.html")
-    context = {}
+    context = {
+        "title": "Question #{}".format(question_id)
+    }
     return HttpResponse(template.render(context, request))
 
 
 def login(request):
     template = loader.get_template("login.html")
-    context = {}
+    context = {
+        "title": "Log In"
+    }
     return HttpResponse(template.render(context, request))
 
 
 def signup(request):
     template = loader.get_template("signup.html")
-    context = {}
+    context = {
+        "title": "Sign Up"
+    }
     return HttpResponse(template.render(context, request))
 
 
 def ask(request):
     template = loader.get_template("ask.html")
-    context = {}
+    context = {
+        "title": "Ask your question"
+    }
     return HttpResponse(template.render(context, request))
 
 def settings(request):
     template = loader.get_template("settings.html")
-    context = {}
+    context = {
+        "title": "Settings"
+    }
     return HttpResponse(template.render(context, request))
